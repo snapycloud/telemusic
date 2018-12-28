a327b1fc47b5cfdb701d7359dbd4e6743850d200
abraham <ig.ghaemi@gmail.com>
2018-12-28 22:57:19 +0330

    update theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 @section('header')
 <header id="home" class="header text-white h-fullscreen text-center text-lg-right" style="background-image: url(../img/bg/32.jpg)">
-      <canvas class="constellation" data-color="rgba(255,255,255,0.3)" width="1920" height="1001"></canvas>
+  <div class="overlay opacity-80" style="background-image: linear-gradient(-45deg, rgba(25, 25, 25, 0.85) 0%, rgba(43, 28, 64, 0.66) 100%);"></div>
+      <canvas class="constellation" data-color="rgba(255,255,255,0.7)" width="1920" height="1001"></canvas>
       <div class="container">
         <div class="row align-items-center h-100">
 
@@ -12,7 +13,7 @@
             <p class="lead mt-5 mb-8">اولین رسانه تصویری اختصاصی موسیقی ایران</p>
             <p class="gap-xy">
               <a class="btn btn-lg btn-round btn-outline-light mw-150" href="#">دربافت اپلیکیشن</a>
-              <a class="btn btn-lg btn-round btn-light mw-150" href="#">ثبت نام</a>
+              <a class="btn btn-lg btn-round btn-light mw-150" href="#" data-toggle="modal" data-target="#modal-register">ثبت نام</a>
             </p>
           </div>
 
@@ -28,9 +29,9 @@
 <section class="section">
         <div class="container">
           <header class="section-header">
-            <h2>Local Video</h2>
+            <h2>تماشا کنید</h2>
             <hr>
-            <p class="lead">While it's always better to host your videos on a video sharing website, but we provided a sample for you to include local videos in your pages.</p>
+            <p class="lead">آنچه در تله موزیک خواهید دید</p>
           </header>
 
 
@@ -38,9 +39,9 @@
             <div class="col-md-8 mx-auto">
 
               <div class="video-wrapper rounded-lg">
-                <div class="poster" style="background-image: url(../img/video/1.jpg)"></div>
+                <div class="poster" style="background-image: url(../img/video/1.png)"></div>
                 <button class="btn btn-circle btn-lg btn-danger"><i class="fa fa-play"></i></button>
-                <video src="../img/video/1.mp4" poster="../img/video/1.jpg" controls=""></video>
+                <video src="https://stream.telemusic.ir/5c192a5a99d396500/backup.mp4" poster="../img/video/1.png" controls=""></video>
               </div>
 
             </div>
@@ -51,97 +52,134 @@
 
 
 
+<section class="section bg-gray">
+        <div class="container">
+          <div class="row gap-y">
 
-<section class="section">
+            <div class="col-md-4">
+              <div class="row">
+                <div class="col-6">
+                  <h4 class="lead-7 text-right counted" data-provide="countup" data-from="0" data-to="42">۲۰،۰۰۰</h4>
+                </div>
+
+                <div class="col-6">
+                  <p class="small text-uppercase ls-2 mb-2">ویدئو‌ها</p>
+                  <p><i class="icon-briefcase lead-4 mb-0"></i></p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="row">
+                <div class="col-6">
+                  <h4 class="lead-7 text-right counted" data-provide="countup" data-from="0" data-to="3500">۳،۵۰۰</h4>
+                </div>
+
+                <div class="col-6">
+                  <p class="small text-uppercase ls-2 mb-2">کاربان محتوا</p>
+                  <p><i class="icon-profile-male lead-4 mb-0"></i></p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="row">
+                <div class="col-6">
+                  <h4 class="lead-7 text-right counted" data-provide="countup" data-from="0" data-to="100" data-prefix="%">۱۰۰٪</h4>
+                </div>
+
+                <div class="col-6">
+                  <p class="small text-uppercase ls-2 mb-2">رضایت</p>
+                  <p><i class="icon-happy lead-4 mb-0"></i></p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      <section class="section">
         <div class="container">
           <header class="section-header">
-            <small>Welcome</small>
-            <h2>Get a Better Understanding</h2>
+            <h2>تله‌موزیک</h2>
             <hr>
-            <p class="lead">تله‌موزیک: اولین رسانه تصویری اختصاصی موسیقی ایران</p>
+            <p class="lead">
+              تله‌موزیک آینه جهان‌نمای دنیای موسیقی است؛ موجوعه‌ای بی‌نظیر از بهترین کنسرت‌ها، فیلم‌های مستند، موزیک ویدیوها، زندگی‌نامه چهره‌های مطرح موسیقی و تاریخ موسیقی ملل که همه آنها در یک اپلیکیشن و روی گوشی شما قابل دسترس است. در حال حاضر نسخه اندروید این اپلیکیشن به صورت آزمایشی منتشر شده است و به زودی نسخه آی‌اواس اپلیکیشن تله‌موزیک هم عرضه خواهد شد
+            </p>
           </header>
 
 
 
           <div class="row gap-y">
 
-            <div class="col-md-8 mx-auto">
-              <img src="../img/preview/dribbble-9.gif" alt="..." data-aos="fade-up" data-aos-duration="2000" class="aos-init aos-animate">
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-mobile lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">پورتال مشتریان</h5>
+              </a>
             </div>
 
 
-            <div class="w-100"></div>
-
-
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-dark mr-5"><i class="icon-mobile"></i></div>
-                <div class="media-body">
-                  <h5>Responsive</h5>
-                  <p>Your website works on any device: desktop, tablet or mobile.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-gears lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">سفارشی سازی آسان</h5>
+              </a>
             </div>
 
 
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-primary mr-5"><i class="icon-gears"></i></div>
-                <div class="media-body">
-                  <h5>Customizable</h5>
-                  <p>You can easily read, edit, and write your own code, or change everything.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-tools lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">سفارشی سازی آسان</h5>
+              </a>
             </div>
 
 
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-info mr-5"><i class="icon-tools"></i></div>
-                <div class="media-body">
-                  <h5>UI Kit</h5>
-                  <p>There is a bunch of useful and necessary elements for developing your website.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-recycle lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">ساده برای استفاده</h5>
+              </a>
             </div>
 
 
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-warning mr-5"><i class="icon-layers"></i></div>
-                <div class="media-body">
-                  <h5>Lego Base</h5>
-                  <p>You can find our code well organized, commented and readable.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-browser lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">پشتیبانی فنی</h5>
+              </a>
             </div>
 
 
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-danger mr-5"><i class="icon-recycle"></i></div>
-                <div class="media-body">
-                  <h5>Clean Code</h5>
-                  <p>As you can see in the source code, we provided a clean code.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-paintbrush lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">پشتیبانی فنی</h5>
+              </a>
             </div>
 
 
-            <div class="col-md-6 col-xl-4">
-              <div class="media">
-                <div class="lead-6 line-height-1 text-success mr-5"><i class="icon-chat"></i></div>
-                <div class="media-body">
-                  <h5>Support</h5>
-                  <p>When you purchase this template, you'll freely receive future updates.</p>
-                </div>
-              </div>
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-puzzle lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">پشتیبانی فنی</h5>
+              </a>
+            </div>
+
+
+            <div class="col-md-6 col-lg-3">
+              <a class="card card-body border hover-shadow-6 text-center py-6" href="#">
+                <p><i class="icon-newspaper lead-6 text-muted"></i></p>
+                <h5 class="card-title text-dark mb-0">پشتیبانی فنی</h5>
+              </a>
             </div>
 
           </div>
-
         </div>
       </section>
+
+
 
 
 
@@ -150,53 +188,15 @@
       <section class="section py-7" style="background-color: #8ea6e6">
         <div class="container">
           <div class="row gap-y align-items-center">
-            <div class="col-md-9">
-              <h4 class="mb-0 text-white text-center text-md-left">Want a bite? You're in the right place!</h4>
-            </div>
-
             <div class="col-md-3 text-center text-md-right">
-              <a class="btn btn-lg btn-round btn-light" href="#">Take a test drive</a>
+              <a class="btn btn-lg btn-round btn-light" href="#" data-toggle="modal" data-target="#modal-register">عضویت در تله موزیک</a>
+            </div>
+            <div class="col-md-9">
+              <h4 class="mb-0 text-white text-center text-md-left">می خوای محتوای ویدئویی وارد کنی؟ از همین جا ثبت نام کن.</h4>
             </div>
           </div>
         </div>
       </section>
-
-
-
-      <section class="section bg-gray text-center">
-        <div class="container">
-
-          <header class="section-header">
-            <small>Try It Now</small>
-            <h2 class="lead-8"><strong>Get Started Free</strong></h2>
-            <hr>
-            <p class="lead">Sign up for free and become one of the millions of people around the world who have fallen in love with GitHub.</p>
-          </header>
-
-
-          <div class="row">
-            <form class="col-md-4 col-xl-4 mx-auto input-border">
-              <div class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Name" autocomplete="off" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
-              </div>
-
-              <div class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Email" autocomplete="off" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
-              </div>
-
-              <div class="form-group">
-                <input type="password" class="form-control form-control-lg" placeholder="Password" autocomplete="off" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
-              </div>
-
-              <button class="btn btn-block btn-xl btn-success">Sign up</button>
-              <p class="small mt-3 opacity-70">or use your <a href="#">Facebook account</a></p>
-            </form>
-          </div>
-
-        </div>
-      </section>
-
-
 
 
 
