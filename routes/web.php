0e281b4ea68cc725fc16926afc23a3727adc6ee4
abraham ghaemi <ig.ghaemi@gmail.com>
2019-02-24 04:52:46 +0330

    set landing to defauklt

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 
-Route::get('/', 'IndexController@getHome')->name('home');
-Route::post('user/register/push', 'UserRegister@postRegister')->name('register');
+// Route::get('/', 'IndexController@getHome')->name('home');
+// Route::post('user/register/push', 'UserRegister@postRegister')->name('register');
 
 
 
-Route::post('/event', 'UserRegister@postOtp')->name('otp.check');
+// Route::post('/event', 'UserRegister@postOtp')->name('otp.check');
 
 
-Route::get('/terms', 'Controller@getLanding')->name('terms');
+// Route::get('/terms', 'Controller@getLanding')->name('terms');
 
 
 
 
 
-Route::get('/landing', 'Controller@getLanding');
+Route::get('/', 'Controller@getLanding');
 Route::post('/send/otp', 'UserRegister@sendClientOtp');
 Route::post('/event/live', 'UserRegister@getEvent');
