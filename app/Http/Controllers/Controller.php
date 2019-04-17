27471fd20ec9655ed3ae268e037dc0c820add7a0
abraham ghaemi <snapycloud@gmail.com>
2019-04-17 11:19:17 +0430

    add phpapi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,12 +6,28 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use SnapyCloud\PhpApi\Client\SnapyClient;
 use Illuminate\Http\Request;
 use App\Telemusic\Api as TelemusicApi;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests,  TelemusicApi;
+
+    protected $client;
+
+    public function __construct()
+    {
+        $url = 'https://server.app.snapycloud.com';
+        $this->client = new SnapyClient($url);
+        $this->client->setApiKey('2434bacac4b4abf96d0d7fb0ec19fc5c');
+        $this->client->setSecretKey('769fce1f0b7264f9c9371f1cb9598662');
+    }
+
+    public function client()
+    {
+        return $this->client;
+    }
 
     public function getLanding()
     {
@@ -34,7 +50,8 @@ class Controller extends BaseController
     		'email' => $request->get('email'),
     		'description' => $request->get('message')
     	];
+    	$ticket = $this->client()->request('POST', 'Case', $data);
 
-    	return $data;
+    	return $ticket;
     }
 }
